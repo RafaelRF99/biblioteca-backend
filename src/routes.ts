827043dc5bfd5ffc39db68b2870e7ad9677159c2
Rafael Ferreira <rafaelferreira@FLABEG.COM.BR>
2023-12-28 09:45:50 -0300

    feat: auth JWT

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,18 @@
 import Livrocontroller from "./controller/Livrocontroller";
+import UserAdmin from "./controller/UserAdmin";
+
 import { Router } from "express";
 
 const routes = Router();
 
-routes.get("/livro", Livrocontroller.getAll)
-routes.post("/livro", Livrocontroller.create)
-routes.put("/livro/:id", Livrocontroller.edit)
-routes.delete("/livro/:id", Livrocontroller.delete)
+routes.get("/livro", Livrocontroller.getAll);
+routes.post("/livro", UserAdmin.verifyJWT, Livrocontroller.create);
+routes.put("/livro/:id", Livrocontroller.edit);
+routes.delete("/livro/:id", Livrocontroller.delete);
+routes.get("/livro/filter", Livrocontroller.filterCategory);
 
-routes.get("/livro/filter", Livrocontroller.filterCategory)
+routes.post("/admin", UserAdmin.valid);
 
-export default routes
+
+
+export default routes;
